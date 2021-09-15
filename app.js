7b021d66faf29d06b32a51a3d1f1ad4720ec90c3
quddufrkawk <quddufrkawk@naver.com>
2021-09-15 20:22:49 +0900

    Initial Commit

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,10 +10,20 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
  const x = event.offsetX;
  const y = event.offsetY;
- console.log(x,y);
+ if(!painting){
+     ctx.beginPath();
+     ctx.moveTo(x, y);
+ } else {
+     ctx.lineTo(x, y);
+     ctx.stroke();
+ }
 }
 
 function onMouseDown(evnet){
@@ -20,13 +34,9 @@ function onMouseUp(event){
     stopPainting();   
 }
 
-function onMouseLeave(event){
-    stopPainting();      
-}
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
